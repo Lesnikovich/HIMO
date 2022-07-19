3bb2a696a893b98493d6ebd7251d3d152201e49f
Lesnikovich <68425826+Lesnikovich@users.noreply.github.com>
2022-07-19 22:00:57 +0300

    HIMO
    Upload HIMO landing page

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+    $('.bike-slider, .slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $(".menu, .bike__column").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		if(window.innerWidth<=1140){
+			$('body,html').animate({scrollTop: top}, 0);
+		} else
+
+		//анимируем переход на расстояние - top за 1500 мс		
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
